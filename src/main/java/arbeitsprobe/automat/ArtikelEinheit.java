55e74a473c5d2eeab94d0d7861d42c8301e38879
David Leska <mail@cronide.de>
2021-10-21 09:07:45 +0200

    Verkaufbare ArtikelEinheit hinzugefuegt

---
+++ src/main/java/arbeitsprobe/automat/ArtikelEinheit.java
@@ -0,0 +1,14 @@
+package arbeitsprobe.automat;
+
+public class ArtikelEinheit {
+
+    private Artikel artikel;
+
+    public ArtikelEinheit(Artikel artikel) {
+        this.artikel = artikel;
+    }
+
+    public Artikel holeArtikel() {
+        return artikel;
+    }
+}
